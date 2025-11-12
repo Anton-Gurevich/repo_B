bafd3b3eb85eabf6901c08d372b6d674329c3038
Anton-Gurevich <antongurevich1989@gmail.com>
2025-11-12 23:06:35 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # repo_B
+A shared lib project example. Should be contained in repo_A as a submodule.
---
+++ src/main.py
@@ -0,0 +1,5 @@
+""" example code for a simple Python application. Should be imported and run from another module. """
+def main():
+    print("Hello from repo_A sub-module!")
+
+
